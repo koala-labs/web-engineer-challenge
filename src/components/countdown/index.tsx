a672ea19f8e726882400955103ca1d35139e3e6d
Annabel Lau <alau@koala.io>
2020-12-16 15:59:35 -0500

    Scaffold app

---
+++ src/components/countdown/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyledCountdown } from './styles';
+
+export const Countdown = () => {
+  // Write your countdown code in this component
+  // Delete the code below
+  return (
+    <StyledCountdown>
+      <h1>Koala Web Engineer Coding Challenge</h1>
+      <h2>Build a countdown clock!</h2>
+      <ul>
+        <li>The only rule is that the clock must take into account hours, minutes, and seconds.</li>
+        <li>This is an imperfect codebase. Feel free to refactor or change anything as you see fit.</li>
+        <li>You're not limited in scope. Be creative and make it as fully fleshed out as you want.</li>
+        <li>Have fun! </li>
+      </ul>
+    </StyledCountdown>
+  );
+};
+
